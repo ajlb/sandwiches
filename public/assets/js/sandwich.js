fa99ccb8b2207320a45e7b975ca895b254e82052
Nancy Lambert-Brown <2643160+n-lambert@users.noreply.github.com>
2020-10-03 19:15:34 -0700

    fix undefined element

--- public/assets/js/sandwich.js
+++ public/assets/js/sandwich.js
@@ -15,7 +15,7 @@ $(function () {
             data: newDevourState
         }).then(
             function () {
-                console.log("changed devour to", newdevour);
+                console.log("changed devour to", newDevour);
                 // Reload the page to get the updated list
                 location.reload();
             }
